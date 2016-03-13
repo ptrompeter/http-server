4a39828103e7fa013319adfd608b6d5f91c10883
ptrompeter <patrick.trompeter@gmail.com>
2016-03-13 12:32:35 -0700

    implemented gevent.

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 setup(
     name="http-server",
     desc="http-server for python 401 week 2",
-    version=0.1,
+    version=1.0,
     author="Patrick Trompeter and Kyle Richardson",
     email="patrick@trompeter.io",
     py_modules=['server', 'client'],
     package_dir={'': 'src'},
-    install_requires=[],
+    install_requires=['gevent'],
     extras_require={'test': ['pytest', 'pytest-xdist', 'tox']}
 )
